3682c3d78da8ab2a8a061d5084807c53369f99c5
saurabh23patre93 <63465162+saurabh23patre93@users.noreply.github.com>
2026-07-15 23:54:26 +0530

    initial Spring boot project setup

---
+++ src/test/java/com/bhojanalay/BhojanalayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhojanalay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhojanalayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
